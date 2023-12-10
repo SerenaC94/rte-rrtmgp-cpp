5cce49493c7022cf5869b3d46c79c86d127a52b3
LukeTheWalker <lucag2001@gmail.com>
2023-12-10 09:59:32 +0100

    cool stuff

--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,6 @@
-cd build && cmake .. -DSYST=ubuntu -DUSECUDA=on && make -j8 && cp test_rte_rrtmgp_gpu ../allsky 
-cd ../allsky
-./make_links.sh
-python allsky_init.py
-python allsky_run.py
+cd build && cmake .. -DSYST=ubuntu -DUSECUDA=on && make -j8 && \
+cd ../allsky && \
+./make_links.sh && \
+python allsky_init.py && \
+python allsky_run.py && \
 python compare-to-reference.py
